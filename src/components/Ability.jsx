be92b6e5176b3aa6e6907b2680d59d884938b737
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-08-06 00:53:40 -0300

    Mobile testv2

--- src/components/Ability.jsx
+++ src/components/Ability.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function Ability({ name, desc, img, onC }) {
   return (
-    <div onClick={onC} className='border border-slate-700 cursor-pointer'>
-      <div className='bg-black'>{name}</div>
-      <img src={img} alt={name}></img>
+    <div className='border border-slate-700 cursor-pointer'>
+      <span onClick={onC}>
+        <div className='bg-black'>{name}</div>
+        <img src={img} alt={name}></img>
+      </span>
       <div className='group border-t-2 bg-black border-slate-700'>
         <span className='font-bold'>?</span>
-        <div className='absolute hidden group-hover:flex bg-black p-3 md:w-fit w-full h-fit max-h-full md:overflow-hidden overflow-y-auto md:min-h-[15rem] -top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-full'>
+        <div className='absolute hidden group-hover:flex bg-black p-3 md:w-fit w-full h-fit max-h-[140%] md:overflow-hidden overflow-y-auto md:min-h-[15rem] -top-1/2 left-1/2 transform -translate-x-1/2 md:-translate-y-full -translate-y-[80%]'>
           <span className='m-auto'>{desc}</span>
         </div>
       </div>
