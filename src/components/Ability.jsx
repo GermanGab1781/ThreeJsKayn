6b52329e92e2ceaca9fde6d46f4680adfe1020df
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-08-06 00:15:04 -0300

    Mobile test

--- src/components/Ability.jsx
+++ src/components/Ability.jsx
@@ -7,7 +7,7 @@ export default function Ability({ name, desc, img, onC }) {
       <img src={img} alt={name}></img>
       <div className='group border-t-2 bg-black border-slate-700'>
         <span className='font-bold'>?</span>
-        <div className='absolute hidden group-hover:flex bg-black p-3 md:w-fit w-full h-fit md:min-h-[15rem] -top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-full'>
+        <div className='absolute hidden group-hover:flex bg-black p-3 md:w-fit w-full h-fit max-h-full md:overflow-hidden overflow-y-auto md:min-h-[15rem] -top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-full'>
           <span className='m-auto'>{desc}</span>
         </div>
       </div>
