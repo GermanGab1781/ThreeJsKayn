026f50725efa448a291e7bbd00c744e088ab3c83
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-10-03 16:06:40 -0300

    Mobile view changes

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -22,7 +22,7 @@ export default function MusicPlayer({ theme }) {
   }
 
   return (
-    <div className='absolute w-32 text-white opacity-60 bg-slate-600 bg-opacity-50 z-50 md:top-2 top-[15%] border px-2 border-black right-[3%] text-center font-Pmarker cursor-pointer select-none' onClick={play}>
+    <div className='absolute w-32 text-white opacity-60 bg-slate-600 bg-opacity-50 z-50 md:top-2 top-9 border px-2 border-black right-[3%] text-center font-Pmarker cursor-pointer select-none' onClick={play}>
       <div className='cursor-pointer'>
         {soundState === 1
           ? (<span className=''>MUSIC: <span className='text-blue-700'>ON</span></span>)
